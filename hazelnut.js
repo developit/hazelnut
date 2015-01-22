814b29d887366f7051f66497af288466b7cb11c2
Jason Miller <jason@developit.ca>
2015-01-22 15:51:41 -0500

    Optimized to get the filesize down to 325 bytes

--- hazelnut.js
+++ hazelnut.js
@@ -2,41 +2,27 @@
 	var modules = {},
 		factories = {};
 
-	function makeRequireFn(relId, module) {
-		return function requireFn(id) {
-			return id==='exports' ? module.exports : (id==='module' ? module : (id==='require' ? requireFn : require(rel(id, relId))));
-		};
-	}
-
 	function require(id) {
 		if (id.pop) id=id[0];
-		var m = modules[id];
-		if (!(id in factories)) throw Error(id + ' not defined');
+		var m = modules[id],
+			e = m && m.exports;
 
 		if (!m) {
-			m = { id: id, exports: {} };
-			modules[id] = m;
-			var requireFn = makeRequireFn(id, m);
-			m.exports = factories[id].apply(m, factories[id].deps.map(requireFn)) || m.exports;
+			m = { id: id, exports:e={} };
+			modules[id] = m.module = m;
+			e = m.exports = factories[id].apply(m, factories[id].deps.map(m.require=function(id) {
+				return m[id] || require(rel(id, m.id));
+			})) || e;
 		}
 
-		return m.exports;
+		return e;
 	}
-	g.require = require;
-	require.config = valueOf;
-	//require.modules = modules;
-	//require.factories = factories;
+	(g.require = require).config = valueOf;
 
 	function define(id, deps, factory) {
-		if (id in factories) throw new Error(id + ' already defined');
-		if (!factory)
-			(factory = deps).deps = [];
-		else
-			factory.deps = deps;
-		factories[id] = factory;
+		(factories[id] = (typeof(factory = factory || deps)!=='function') ? function(){return factory;} : factory).deps = deps.pop ? deps : [];
 	}
-	define.amd = {};
-	g.define = define;
+	(g.define = define).amd = {};
 
 	function rel(name, path) {
 		name = name.replace(/^(?:\.\/|(\.\.\/))/, path.replace(/[^\/]+$/g,'') + '$1');
